c766e5302d35468173c06039ca74ff147d6d1d46
Lynxvn <englishlynx194@gmail.com>
2023-07-01 23:12:05 +1000

    Hết bài 11

--- Lesson11.js
+++ Lesson11.js
@@ -1,4 +1,14 @@
 // If the hour is less than 18, create a "Good day" greeting, otherwise "Good evening":
-var day= new Date()
+var day = new Date()
 var hours = day.getHours()
+const a = (b) => {
+    if (b < 18) {
+        return "Good day"
+    } else {
+        return "Good evening"
+    }
+}
+
+var c = a(hours)
+console.log(c)
 console.log(hours)
