fc7f024c945297fc35c97932e315b1b009c95216
Lynxvn <englishlynx194@gmail.com>
2023-07-08 23:33:00 +1000

    Fix lesson12

--- lesson12.js
+++ lesson12.js
@@ -15,22 +15,17 @@ const salaryinweekends = (hours1) => {
 const salaryperweek = (hours, hours1) => {
     return salaryinweekdays(hours) + salaryinweekends(hours1);
 }
-// var f= salaryperweek (20,8)
-// console.log(f)
-// var salaryperweek1 = salaryperweek(2,3)
-// console.log(salaryperweek1)
+
 
 const salarypermonth = (hours, hours1) => {
     return salaryperweek(hours, hours1) * 4;
 }
-// var c = salarypermonth(20, 8)
-// console.log(c)
+
 
 const roombill = (roombillperweek) => {
     return roombillperweek * 4;
 }
-// var d = roombill(250)
-// console.log(d)
+
 
 const money = (hours,hours1,roombillperweek)=>{
     return salarypermonth(hours,hours1)-roombill(roombillperweek);
