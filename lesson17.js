a7f34ac095a10ffa2ae098c04dfe24a46d7ecb47
Lynxvn <englishlynx194@gmail.com>
2023-08-14 23:25:36 +1000

    lesson17.js

---
+++ lesson17.js
@@ -0,0 +1,93 @@
+// while tương tự for loop 
+
+// while (condition){
+// code here 
+// }
+
+// console.log("foor loop 1 to 5")
+
+// for (i = 1; i <= 5; i++)
+//     console.log(i)
+
+// ---------------------------
+// a = 1
+// while (a <= 5) {
+//     console.log(a)
+//     a++;
+// }
+
+// do .. while 
+// thuc hien ít nhất một dòng code trước khi kt điều kiện
+
+// let  b = 1;
+// do {
+//     console.log(b);
+//     b++;
+// } while (i>=5);
+
+
+// ---------------------------------------------
+
+// x = 0
+// switch (x) {
+//     case 0:
+//         text = "off"
+//         break;
+//     case 1:
+//         text = "On"
+//         break;
+//     default:
+//         text = "NO value found";
+// }
+
+// console.log(text)
+
+
+// ---------------------- Exercise------------------------
+// cho người dùng enter ngày (1-7 )
+// xác định ngày off và work và study dưa vào số ngày làm việc
+// const prompt = require("prompt-sync")({ sigint: true })
+// a = parseInt(prompt("Ngày "))
+// switch (a) {
+//     case 1:
+//      text = "off"
+//         break;
+//     case 2:
+//         text = "work"
+//         break;
+//     case 5:
+//         text = "off"
+//         break;
+//     case 7:
+//         text = "work"
+//         break;
+//     default:
+//         text = "study";
+// }
+// console.log(text)
+
+
+// const prompt = require("prompt-sync")({ sigint: true })
+// a = parseInt(prompt("Ngày "))
+// switch (a) {
+//     case 1:
+//     case 5:
+//         text = "off"
+//         break;
+//     case 2:
+//     case 7:
+//         text = "work"
+//         break;
+//     default:
+//         text = "study";
+// }
+// console.log(text)
+
+
+// 
+// select shopping , game , study 
+// show bán hàng thuoc mảng trên 
+
+for (i = 1; i<Infinity; i++) {
+    console.log(i)
+}
