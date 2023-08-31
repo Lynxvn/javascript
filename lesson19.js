cc0429a7255fbb7e06501974ac5f0d61def11555
Lynxvn <englishlynx194@gmail.com>
2023-08-31 23:42:22 +1000

    lesson19

--- lesson19.js
+++ lesson19.js
@@ -1,13 +1,13 @@
 // Bài tập 1: Tính bình phương của các phần tử trong một mảng
-
+// Cach 1:
 // const number=[2,3,4,5]
 // const binhPhuong=number.map(number => Math.pow(number,2))
 // console.log(binhPhuong)
-
+// Cach 2
 // const number=[2,3,4,5]
 // const binhPhuong=number.map(number => number*number)
 // console.log(binhPhuong)
-
+// Cach 3
 // const numbers=[2,3,4,5]
 // function binhPhuong(a){
 //     return a*a
@@ -15,10 +15,10 @@
 // const ketQua=numbers.map(binhPhuong)
 // console.log(ketQua)
 
-const numbers=[2,3,4,5]
-const binhPhuong = (a) => {
-        return a*a
-    }
-    const ketQua= numbers.map(binhPhuong)
-    console.log(ketQua)
+// const numbers=[2,3,4,5]
+// const binhPhuong = (a) => {
+//         return a*a
+//     }
+//     const ketQua= numbers.map(binhPhuong)
+//     console.log(ketQua)
 // Yêu cầu: Viết một hàm sử dụng Map để tính bình phương của từng phần tử trong một mảng và trả về mảng mới chứa kết quả.
