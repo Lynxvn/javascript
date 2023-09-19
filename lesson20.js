2285400095ed7fa0dc408d17f60238ad5269960a
Lynxvn <englishlynx194@gmail.com>
2023-09-20 01:46:05 +1000

    bài 20

--- lesson20.js
+++ lesson20.js
@@ -44,19 +44,52 @@ const jobList = [
   { id: 2, title: 'Đọc sách', completed: true },
   { id: 3, title: 'Viết email', completed: false },
   { id: 4, title: 'Học guitar', completed: true },
-]
-const job1 = { id: 1, title: 'Làm bài tập lập trình', completed: false }
+];
+const job1 = {id: 1, title: 'Làm bài tập lập trình', completed: false}
 const job2 = { id: 2, title: 'Đọc sách', completed: true }
 const job3 = { id: 3, title: 'Viết email', completed: false }
-const job4 = { id: 4, title: 'Học guitar', completed: true }
+const job4 ={ id: 4, title: 'Học guitar', completed: true }
+//Sài map
+const job1a =
+  { id: 1, title: 'Làm bài tập lập trình', status: job1.completed }
+  const a = (b) => {
+    if (b==true){
+      return {id: 1, title: 'Làm bài tập lập trình', status:"Xong òi ạ"}
+    }else{ return {id: 1, title: 'Làm bài tập lập trình', status:"chưa má ơi"}} }
+  const job1b=a(job1.completed)
+  //console.log (job1b)
 
-const newjob1 = { id: 1, title: 'Làm bài tập lập trình', status: job1.completed }
-const newjob2 = { id: 2, title: 'Đọc sách', status: job2.completed }
-const newjob3 = { id: 3, title: 'Viết email', status: job3.completed }
-const newjob4 = { id: 4, title: 'Học guitar', status: job4.completed }
 
-Sài map
 
-const jobList2 = [newjob1, newjob2, newjob3, newjob4]
-//Array = [Object {key: value}]
+  const job2a = { id: 2, title: 'Đọc sách', status: job2.completed }
+  const d = (e) => {
+    if (e==true){
+      return {id: 2, title: 'Đọc sách', status:"Xong òi ạ"}
+    }else{ return {id: 2, title: 'Đọc sách', status:"chưa má ơi"}} }
+  const job2b=a(job2.completed)
+  //console.log (job2b)
 
+
+
+  const job3a = { id: 3, title: 'Viết email', status: job3.completed }
+  const f = (h) => {
+    if (h==true){
+      return { id: 3, title: 'Viết email', status:"Xong òi ạ"}
+    }else{ return { id: 3, title: 'Viết email', status:"chưa má ơi"}} }
+  const job3b=a(job3.completed)
+  //console.log (job3b)
+
+
+
+
+  const job4a = { id: 4, title: 'Học guitar', status: job4.completed }
+const e = (g) => {
+    if (g==true){
+      return { id: 4, title: 'Học guitar', status:"Xong òi ạ"}
+    }else{ return { id: 4, title: 'Học guitar', status: "chưa má ơi"}} }
+  const job4b=a(job4.completed)
+  console.log (job4b)
+
+  jobList2 = [job1b,job2b,job3b,job4b]
+const change = jobList2.map(key => ({ id: key.id , title: key.title, status: key.status}))
+//console.log(change)
