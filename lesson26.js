67564fe0ee2e2785b8ec1f9ffbaa602bafa188ba
Lynxvn <englishlynx194@gmail.com>
2023-10-17 00:57:57 +1100

    lesson26

---
+++ lesson26.js
@@ -0,0 +1,23 @@
+// DẠNG ARRAY
+//1. Tạo một array bất kỳ (chữ or số) thêm một phần tử mới 
+//2. Loại bỏ phần tử cuối cùng
+//3. Loại bỏ phần tử thứ 2
+//4. Đếm phần tử trong mảng
+//5. Tìm vị trí của 1 giá trị bất kỳ 
+//6. Tạo một array về số bất kỳ 
+//7. Lọc các phần tử số chẳn
+//8. Tìm số lớn nhất trong mảng
+//9. Lọc số lẻ và tính bình phương của chúng
+
+const batKy = [1,2,3,"bốn","năm","sáu"]
+const boCuoi = batKy.pop()
+console.log (batKy) //=> [ 1, 2, 3, 'bốn', 'năm' ]
+console.log (boCuoi) // => sáu
+
+//DẠNG OBJECT
+//1. Tạo một đối tượng bao gồm Tên, tuổi, địa chỉ, thu nhập 
+//2. Truy cập thuộc tính bất kỳ 
+//3. Thay đổi giá trị thuộc tính
+//4. Xóa thuộc tính đối tượng
+//5. In tra các key của đối tượng 
+//6. Kiểm tra 
