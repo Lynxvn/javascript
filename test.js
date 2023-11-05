f360496cfad7731c4ca5f5ff0f13a180b44a19bc
Lynxvn <englishlynx194@gmail.com>
2023-11-05 21:26:51 +1100

    Co-authored-by: Kim Kim  <selenalee123@users.noreply.github.com>

---
+++ test.js
@@ -0,0 +1,29 @@
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// const evens = numbers.filter((number) => number % 2 == 0)
+// console.log(evens)
+
+// const people = [
+//         {"name": "A", "age": 10},
+//         {"name": "B", "age": 19},
+//         {"name": "C", "age": 7},
+//     ];
+// const names = people.map((ten) => ten.name)
+//         console.log(names)
+
+//  const people = [ {"name":"Hoa"}, {"name":"Đào"}, {"name":"Nụ"}, {"name":"Thắm"}
+//          ];
+// const names = people.map((ten) => "hello " + ten.name )
+//         console.log(names)
+
+// const students = [
+//     {"name": "A", "age": 10, "grade":5},
+//     {"name": "B", "age": 18,"grade":12 },
+//     {"name": "C", "age": 7, "grade":3},]
+
+// const people = [
+//         {"name": "A", "salary": 60000},
+//         {"name": "B", "salary": 18000},
+//         {"name": "C", "salary": 70000},
+//     ];
+// const names = people.filter((ten) => ten.salary <50000)
+//         console.log(names)
